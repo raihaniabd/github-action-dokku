31cdc3f1fc0e056164fc2d6d14f13cb229f96845
abdessamad raihani <Abdessamadraih@gmail.com>
2021-12-16 23:17:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,4 @@ ssh-add "$SSH_PATH/deploy_key"
 
 ssh-keyscan -t rsa "$INPUT_HOST" >> "$SSH_PATH/known_hosts"
 
-git checkout "$INPUT_BRANCH"
-
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push -f dokku@"$INPUT_HOST":"$INPUT_PROJECT" "$INPUT_BRANCH"
