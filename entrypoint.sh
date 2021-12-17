5c09fca6cf5547a77c3cb674f2f4c23a6a3e1d63
abdessamad raihani <Abdessamadraih@gmail.com>
2021-12-17 02:28:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,10 +26,8 @@ git remote -v
 
 git branch
 
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" 
-
 DOKKU_APP_URL=ssh://dokku@"$INPUT_HOST"/"$INPUT_PROJECT"
 
 echo "$DOKKU_APP_URL"
 
-git push "$DOKKU_APP_URL" "$INPUT_BRANCH"
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push "$DOKKU_APP_URL" "$INPUT_BRANCH"
